247427ba9448921681b6583b77d2bde46d8cf8e9
Zhmylev Alexey <bezblagodatnost@gmail.com>
2018-08-17 09:47:52 +0300

    Initial commit

---
+++ src/main/java/ru/otus/bookstoreweb/BookstoreWebApplication.java
@@ -0,0 +1,12 @@
+package ru.otus.bookstoreweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreWebApplication.class, args);
+    }
+}
