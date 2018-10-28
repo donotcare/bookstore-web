a599f28a5ff3e55510f685bca33d69829508fd07
Zhmylev Alexey <bezblagodatnost@gmail.com>
2018-10-29 00:36:11 +0300

    added acl spring security

---
+++ src/main/java/ru/otus/bookstoreweb/security/MapUserDetailsService.java
@@ -0,0 +1,22 @@
+package ru.otus.bookstoreweb.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Map;
+
+public class MapUserDetailsService implements UserDetailsService {
+    private final Map<String, UserDetails> userDetailsByUserName;
+    public MapUserDetailsService(Map<String, UserDetails> userDetailsByUserName) {
+        this.userDetailsByUserName = userDetailsByUserName;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        UserDetails userDetails = userDetailsByUserName.get(userName);
+        if(userDetails == null) {
+            throw new UsernameNotFoundException("Username " + userName + " was not found");
+        }
+        return userDetails;
+    }
+}
---
+++ src/main/java/ru/otus/bookstoreweb/security/SecurityAclConfiguration.java
@@ -0,0 +1,20 @@
+package ru.otus.bookstoreweb.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SecurityAclConfiguration extends GlobalMethodSecurityConfiguration {
+
+    @Override
+    protected MethodSecurityExpressionHandler createExpressionHandler() {
+        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+        expressionHandler.setPermissionEvaluator(new CustomPermissionEvaluator());
+        return expressionHandler;
+    }
+
+}
